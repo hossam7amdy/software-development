8663740b15b8b75c6489d22655481938a7647155
hossam7amdy <hossamhamdy117@gmail.com>
2024-01-03 14:59:20 +0200

    remove useCallback hook

--- 06-react-patterns/3-HOC-pattern/src/hooks/useFetch.tsx
+++ 06-react-patterns/3-HOC-pattern/src/hooks/useFetch.tsx
@@ -1,17 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useFetch = (url: string) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
-  const fetchData = useCallback(
-    (url: string) => fetch(url).then((res) => res.json()),
-    []
-  );
-
   useEffect(() => {
-    fetchData(url)
+    fetch(url)
+      .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => setError(err))
       .finally(() => setLoading(false));

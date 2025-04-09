bceb50a434c6b678059190cb83c249ac746f7b67
Hossam Hamdy <hossamhamdy117@gmail.com>
2025-04-10 01:27:52 +0200

    Introduce typescript to the project

---
+++ 07-node-design-patterns/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['standard', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  rules: {
+    strict: 1
+  }
+}

fe0049c73d9235d290ce8813d95cacb5fd52dd55
Hossam Hamdy <hossamhamdy117@gmail.com>
2025-05-04 23:35:57 +0300

    feat: update test logging functions to include clearer output for warnings without timestamps

--- 07-node-design-patterns/08-structural-design-patterns/ex2-timestamped-logs/index.ts
+++ 07-node-design-patterns/08-structural-design-patterns/ex2-timestamped-logs/index.ts
@@ -5,33 +5,36 @@ import {
 } from './timestamped-logs'
 
 const testTimestampedLogsProxy = () => {
+  console.log('\n========== Testing Proxy =============')
   const logger = timestampedLogsProxy(console)
 
   logger.log('Proxy:', 'log')
   logger.debug('Proxy:', 'debug')
   logger.error('Proxy:', 'error')
   logger.info('Proxy:', 'info')
-  logger.warn('Proxy:', 'warn')
+  logger.warn('Proxy (no timestamp):', 'warn')
 }
 
 const testTimestampLogsAugmentation = () => {
+  console.log('\n========== Testing Augmentation =============')
   const logger = timestampLogsAugmentation(console)
 
   logger.log('Augmentation:', 'log')
   logger.debug('Augmentation:', 'debug')
   logger.error('Augmentation:', 'error')
   logger.info('Augmentation:', 'info')
-  logger.warn('Augmentation:', 'warn')
+  logger.warn('Augmentation (no timestamp):', 'warn')
 }
 
 const testTimestampLogsComposition = () => {
+  console.log('\n========== Testing Composition =============')
   const logger = timestampLogsComposition(console)
 
   logger.log('Composition:', 'log')
   logger.debug('Composition:', 'debug')
   logger.error('Composition:', 'error')
   logger.info('Composition:', 'info')
-  logger.warn('Composition:', 'warn')
+  logger.warn('Composition (no timestamp):', 'warn')
 }
 
 testTimestampLogsComposition()
